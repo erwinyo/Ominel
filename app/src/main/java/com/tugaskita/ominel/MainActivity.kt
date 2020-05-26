a159fc0ed14da093ac0dbcd28638f3d20d0223ea
erwinyo <erwinwingyonata@gmail.com>
2020-05-26 22:57:08 +0700

    Second Commit

--- app/src/main/java/com/tugaskita/ominel/MainActivity.kt
+++ app/src/main/java/com/tugaskita/ominel/MainActivity.kt
@@ -1,17 +1,46 @@
 package com.tugaskita.ominel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    // Default is Google Search Engine
+    var sev = "https://google.com/search?q="
+    var keyword = ""
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Enabling JavaScript from webview
+        web.settings.javaScriptEnabled = true
+        // Buat webview tidak redirect ke default browser, tetap di dalam aplikasi
+        web.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url)
+                return false // then it is not handled by default action
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // Hide Progress Bar
+                progressBar.visibility = View.INVISIBLE
+                super.onPageFinished(view, url)
+            }
+        }
 
         searchbox.addTextChangedListener(object : TextWatcher {
 
@@ -25,8 +54,56 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence, start: Int,
                                        before: Int, count: Int) {
-                web.loadUrl("https://google.com/search?q=$s")
+
+                // Load Every Word Typed & Save the value to 'keyword' global variable
+                keyword = "$s"
+                web.loadUrl("${sev.toLowerCase(Locale.US)}$keyword")
+                // Show Progress Bar
+                progressBar.visibility = View.VISIBLE
             }
         })
+    }
+
+    fun selectSearchEngineDialog(view: View) {
+        val items = arrayOf("Google", "DuckDuckGo", "Bing", "Yahoo")
+        val itemIcons =
+            arrayOf(R.drawable.google, R.drawable.duckduckgo, R.drawable.bing, R.drawable.yahoo)
+        val itemUrls = arrayOf(
+            "https://google.com/search?q=",
+            "https://duckduckgo.com/?q=",
+            "https://www.bing.com/search?q=",
+            "https://search.yahoo.com/search?p="
+        )
+        with(AlertDialog.Builder(this))
+        {
+            setTitle("Select the search engine that you want")
+            setItems(items) { dialog, index ->
+                // Update sev (Search Engine Value) global variable value
+                sev = itemUrls[index]
+
+                // Change the search engine icon
+                searchengine.setImageResource(itemIcons[index])
+
+                // Reload webview after search engine selection has been made
+                web.loadUrl("${sev.toLowerCase(Locale.US)}$keyword")
+
+                // Toasted for user changes
+                Toast.makeText(
+                    applicationContext,
+                    "${items[index]}'s search engine has been chosen",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            setPositiveButton("OK", null)
+            show()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (web.canGoBack())
+            web.goBack()
+        else
+            finish()
     }
 }
--- app/src/main/java/com/tugaskita/ominel/SplashActivity.kt
+++ app/src/main/java/com/tugaskita/ominel/SplashActivity.kt
@@ -1,20 +1,19 @@
 package com.tugaskita.ominel
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     private val mWaitHandler: Handler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        mWaitHandler.postDelayed(Runnable {
-            //The following code will execute after the 5 seconds.
+        mWaitHandler.postDelayed({
+            //The following code will execute after the 3 seconds.
             try { //Go to next page i.e, start the next activity.
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intent)
+                startActivity(Intent(applicationContext, MainActivity::class.java))
                 //Let's Finish Splash Activity since we don't want to show this when user press back button.
                 finish()
             } catch (ignored: Exception) {
